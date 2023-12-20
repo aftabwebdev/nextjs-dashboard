114bf08e4e0366cf1a07212cdfc619d9da779deb
Aftab <aftab2060@gmail.com>
2023-12-20 20:15:24 +0600

    first init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Nunito, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const nunito = Nunito({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
